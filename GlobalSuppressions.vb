11a41645467254e50f891e64ee9fe90b07bb9ee7
magbender <58702100+jalepeno@users.noreply.github.com>
2025-01-17 11:06:48 -0600

    Add files via upload

---
+++ GlobalSuppressions.vb
@@ -0,0 +1,20 @@
+﻿' This file is used by Code Analysis to maintain SuppressMessage
+' attributes that are applied to this project.
+' Project-level suppressions either have no target or are given
+' a specific target and scoped to a namespace, type, member, etc.
+
+Imports System.Diagnostics.CodeAnalysis
+
+<Assembly: SuppressMessage("Design", "CA1061:Do not hide base class methods", Justification:="<Pending>", Scope:="member", Target:="~M:Documents.Files.FileDefinitions.Add(Documents.Files.IFileDefinition)")>
+<Assembly: SuppressMessage("Design", "CA1061:Do not hide base class methods", Justification:="<Pending>", Scope:="member", Target:="~M:Documents.Security.AccessRights.Add(Documents.Security.IAccessMask)")>
+<Assembly: SuppressMessage("Design", "CA1061:Do not hide base class methods", Justification:="<Pending>", Scope:="member", Target:="~M:Documents.Files.FileDefinitions.Insert(System.Int32,Documents.Files.IFileDefinition)")>
+<Assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification:="<Pending>", Scope:="member", Target:="~M:Documents.Transformations.TransformAllRelatedDocumentsAction.TransformAllDocuments(Documents.Transformations.Transformation)~Documents.Transformations.ActionResult")>
+<Assembly: SuppressMessage("Design", "CA1061:Do not hide base class methods", Justification:="<Pending>", Scope:="member", Target:="~M:Documents.Files.FileDefinitions.Contains(Documents.Files.IFileDefinition)~System.Boolean")>
+<Assembly: SuppressMessage("Design", "CA1061:Do not hide base class methods", Justification:="<Pending>", Scope:="member", Target:="~M:Documents.Security.AccessRights.Contains(Documents.Security.IAccessMask)~System.Boolean")>
+<Assembly: SuppressMessage("Design", "CA1061:Do not hide base class methods", Justification:="<Pending>", Scope:="member", Target:="~M:Documents.Files.FileDefinitions.IndexOf(Documents.Files.IFileDefinition)~System.Int32")>
+<Assembly: SuppressMessage("Design", "CA1061:Do not hide base class methods", Justification:="<Pending>", Scope:="member", Target:="~M:Documents.Files.FileDefinitions.Remove(Documents.Files.IFileDefinition)~System.Boolean")>
+<Assembly: SuppressMessage("Design", "CA1061:Do not hide base class methods", Justification:="<Pending>", Scope:="member", Target:="~M:Documents.Security.AccessRights.Remove(Documents.Security.IAccessMask)~System.Boolean")>
+<Assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification:="<Pending>", Scope:="member", Target:="~M:Documents.Encryption.Asymmetric.GetRSAProvider~System.Security.Cryptography.RSACryptoServiceProvider")>
+<Assembly: SuppressMessage("Design", "CA1061:Do not hide base class methods", Justification:="<Pending>", Scope:="member", Target:="~M:Documents.Transformations.Actions.Add(Documents.Transformations.ITransformationAction)")>
+<Assembly: SuppressMessage("Design", "CA1061:Do not hide base class methods", Justification:="<Pending>", Scope:="member", Target:="~M:Documents.Transformations.Actions.Contains(Documents.Transformations.ITransformationAction)~System.Boolean")>
+<Assembly: SuppressMessage("Design", "CA1061:Do not hide base class methods", Justification:="<Pending>", Scope:="member", Target:="~M:Documents.Transformations.Actions.Remove(Documents.Transformations.ITransformationAction)~System.Boolean")>
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Documents
-Ecmg.Documents.dll is a .NET library for abstracting documents, versions and metadata.  It is a free version of a portion of the original Content Transformation Services framework.
+﻿Ecmg.Documents.dll is a .NET Core library for abstracting documents, versions and metadata.  It is a free version of a portion of the original Content Transformation Services framework.
